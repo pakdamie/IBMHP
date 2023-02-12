5f3157c4494f6ea5eede5e4796d0e471af593e94
unknown <dkp5177@psu.edu>
2023-02-11 19:13:35 -0500

    replaced == with %in% and got rid of some of the errors- should work now

--- code.R
+++ code.R
@@ -6,7 +6,7 @@ library(ggplot2)
 
 ###Let's say this is a data.frame of what it would look like
 
-###Let's try getting them to move?
+###Movement of all agents 
 Movement<- function(Pop){
 
   move_x <- sample(x = c(-1, 0, 1), size = nrow(Pop),replace=TRUE)
@@ -46,6 +46,7 @@ Movement<- function(Pop){
   
 }
 
+###Infection of the RBC by a parasite 
 Infection <- function(Pop){
   ###RBC
   RBC <-  subset(Pop, Pop$type=='R')
@@ -60,13 +61,13 @@ Infection <- function(Pop){
    RBC_ind <- Same_spots$individuals.x
    Parasite_ind <- Same_spots$individuals.y
     
-   RBC[RBC$individuals== RBC_ind ,]$type<- "I"
-   RBC[RBC$individuals== RBC_ind,]$infected<- "T"
-   RBC[RBC$individuals== RBC_ind,]$time_infected <- 1
+   RBC[RBC$individuals %in% RBC_ind ,]$type<- "I"
+   RBC[RBC$individuals%in% RBC_ind,]$infected<- "T"
+   RBC[RBC$individuals%in%RBC_ind,]$time_infected <- 1
   
-   Parasite[Parasite$individuals==Parasite_ind ,]$infect <-"T"
-   Parasite[Parasite$individuals==Parasite_ind ,]$x <- NA
-   Parasite[Parasite$individuals== Parasite_ind ,]$y <- NA
+   Parasite[Parasite$individuals%in%Parasite_ind ,]$infect <-"T"
+   Parasite[Parasite$individuals%in%Parasite_ind ,]$x <- NA
+   Parasite[Parasite$individuals%in% Parasite_ind ,]$y <- NA
    
    Pop <- rbind(RBC,Parasite,Infected_RBC)
    Pop  <- Pop[order( Pop $individuals),]
@@ -81,9 +82,7 @@ Infection <- function(Pop){
   
 }
 
-###Each time step is 5 minutes
-###Merozoite will die in 6 minutes (3 time step)
-
+###The natural death of a parasite if failing to infect
 Death <- function(Pop){
 
   RBC <-     subset(Pop, Pop$type=='R')
@@ -114,8 +113,7 @@ return(Pop)
   
 }
 
-
-###Burst 
+###Bursting of infected RBC  
 
 Burst <- function(Pop){
   ###RBC
@@ -126,7 +124,7 @@ Burst <- function(Pop){
   Infected_RBC <- subset(Pop,Pop$type=='I')
   
   #Bye Bye
-Burst <- subset(Infected_RBC,  Infected_RBC$time_infected > & Infected_RBC$dead == "F")
+Burst <- subset(Infected_RBC,  Infected_RBC$time_infected >5 & Infected_RBC$dead == "F")
   
   if(nrow(Burst) > 0){
     Burst_Ind <-  Burst$individuals
@@ -137,8 +135,8 @@ Burst <- subset(Infected_RBC,  Infected_RBC$time_infected > & Infected_RBC$dead 
     New_Parasites <- NULL
     for (ind in seq(1,length(Burst_Ind))){
       ind_no <- Burst_Ind[ind]
-      x_pos <-  Infected_RBC[Infected_RBC$individual ==ind_no,]$x
-      y_pos <-  Infected_RBC[Infected_RBC$individual ==ind_no,]$y
+      x_pos <-  Infected_RBC[Infected_RBC$individual%in%ind_no,]$x
+      y_pos <-  Infected_RBC[Infected_RBC$individual %in%ind_no,]$y
       
       
       move_x <- sample(x = c(-1, 0, 1), size = 5,replace=TRUE)
@@ -215,7 +213,7 @@ return(Pop)
 Pop<- 
   data.frame(
     individuals = seq(1,200),
-    type = c(rep("R",150), rep("P",50)),
+    type = c(rep("R",180), rep("P",20)),
     x = sample.int(100, 200,replace=TRUE),
     y = sample.int(100, 200,replace=TRUE),
     time_alive = rep(0,200),
@@ -236,7 +234,7 @@ for (k in seq(1,100)){
   Pop <- Death(Pop)
   Pop <- Infection(Pop)
   Pop <- Movement(Pop)
-  Pop$time_alive <- Pop$time_alive +1
+  Pop[Pop$dead=="F",]$time_alive<- Pop[Pop$dead=="F",]$time_alive +1
   Pop[Pop$time_infected!=0,]$time_infected <-  Pop[Pop$time_infected!=0,]$time_infected+1
   
 
@@ -247,12 +245,10 @@ for (k in seq(1,100)){
 
 Pop_All <- do.call(rbind,full_list )
 
-b<- ggplot(Pop_All, aes(x= x, y= y,fill=type))+
+animation<- ggplot(Pop_All, aes(x= x, y= y,fill=type))+
   geom_point(size=4,shape=21)+
   scale_fill_manual(name="",label=c("I"='Infected RBC', "R"='RBC', "P"= 'Merozoite'),
-                     values=c("I"='#74f76d','R'='#d95981', 'P'='#97c7f7')
-                     
-                     )+
-                              transition_time(time)+ theme_void()+ labs(title = 'Minutes: {frame_time}')
+                     values=c("I"='#74f76d','R'='#d95981', 'P'='#97c7f7'))+
+  transition_time(time)+ theme_void()+ labs(title = 'Minutes: {frame_time}')
 
-anim_save('b.gif', animation = last_animation())
+#anim_save('animation.gif', animation = last_animation())
